3335617b120613f77af768c332704bdce6d60cb5
effiongcharles <talkwithcharles@gmail.com>
2022-04-20 22:23:00 +0200

    first commit

---
+++ js/db.js
@@ -0,0 +1,232 @@
+const questions = [
+  {
+    id: 1,
+    question:
+      "You're really busy at work and a colleague is telling you their life story and personal woes. You:",
+    answers: [
+      {
+        text: "Don't dare to interrupt them",
+        cat: "introvert",
+      },
+      {
+        text: "Think it's more important to give them some of your time; work can wait",
+        cat: "extrovert",
+      },
+      {
+        text: "Listen, but with only with half an ear",
+        cat: "introvert",
+      },
+      {
+        text: "Interrupt and explain that you are really busy at the moment",
+        cat: "extrovert",
+      },
+    ],
+  },
+  {
+    id: 2,
+    question:
+      "You've been sitting in the doctor's waiting room for more than 25 minutes. You:",
+    answers: [
+      {
+        text: "Bubble with inner anger, but keep quiet",
+        cat: "introvert",
+      },
+      {
+        text: "Look at your watch every two minutes",
+        cat: "introvert",
+      },
+      {
+        text: "Explain to other equally impatient people in the room that the doctor is always running late",
+        cat: "extrovert",
+      },
+      {
+        text: "Complain in a loud voice, while tapping your foot impatiently",
+        cat: "extrovert",
+      },
+    ],
+  },
+  {
+    id: 3,
+    question:
+      "You're having an animated discussion with a colleague regarding a project that you're in charge of. You:",
+    answers: [
+      {
+        text: "Don't dare contradict them",
+        cat: "introvert",
+      },
+      {
+        text: "Defend your own point of view, tooth and nail",
+        cat: "extrovert",
+      },
+      {
+        text: "Think that they are obviously right",
+        cat: "introvert",
+      },
+      {
+        text: "Continuously interrupt your colleague",
+        cat: "extrovert",
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "You are taking part in a guided tour of a museum. You:",
+    answers: [
+      {
+        text: "Are a bit too far towards the back so don't really hear what the guide is saying",
+        cat: "introvert",
+      },
+      {
+        text: "Make sure that everyone is able to hear properly",
+        cat: "extrovert",
+      },
+      {
+        text: "Make sure that everyone is able to hear properly",
+        cat: "introvert",
+      },
+      {
+        text: "Are right up the front, adding your own comments in a loud voice",
+        cat: "extrovert",
+      },
+    ],
+  },
+  {
+    id: 5,
+    question:
+      "During dinner parties at your home, you have a hard time with people who:",
+    answers: [
+      {
+        text: "Ask you to tell a story in front of everyone else",
+        cat: "introvert",
+      },
+      {
+        text: "Talk privately between themselves",
+        cat: "extrovert",
+      },
+      {
+        text: "Hang around you all evening",
+        cat: "introvert",
+      },
+      {
+        text: "Always drag the conversation back to themselves",
+        cat: "extrovert",
+      },
+    ],
+  },
+  {
+    id: 6,
+    question:
+      "You crack a joke at work, but nobody seems to have noticed. You:",
+    answers: [
+      {
+        text: "Think it's for the best — it was a lame joke anyway",
+        cat: "introvert",
+      },
+      {
+        text: "Keep telling it until they pay attention",
+        cat: "extrovert",
+      },
+      {
+        text: "Try again a bit later with one of your colleagues",
+        cat: "introvert",
+      },
+      {
+        text: "Wait to share it with your friends after work",
+        cat: "extrovert",
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "This morning, your agenda seems to be free. You:",
+    answers: [
+      {
+        text: "Have a sigh of relief and look forward to a day without stress",
+        cat: "introvert",
+      },
+      {
+        text: "Question your colleagues about a project that's been worrying you",
+        cat: "extrovert",
+      },
+      {
+        text: "Know that somebody will find a reason to come and bother you",
+        cat: "introvert",
+      },
+      {
+        text: "Pick up the phone and start filling up your agenda with meetings",
+        cat: "extrovert",
+      },
+    ],
+  },
+  {
+    id: 8,
+    question:
+      "During dinner, the discussion moves to a subject about which you know nothing at all. You:",
+    answers: [
+      {
+        text: "Barely follow the discussion",
+        cat: "introvert",
+      },
+      {
+        text: "Ask lots of questions to learn more about it",
+        cat: "extrovert",
+      },
+      {
+        text: "Don't dare show that you don't know anything about the subject",
+        cat: "introvert",
+      },
+      {
+        text: "Change the subject of discussion",
+        cat: "extrovert",
+      },
+    ],
+  },
+  {
+    id: 9,
+    question:
+      "You're out with a group of friends and there's a person who's quite shy and doesn't talk much. You:",
+    answers: [
+      {
+        text: "Notice that they're alone, but don't go over to talk with them",
+        cat: "introvert",
+      },
+      {
+        text: "Go and have a chat with them",
+        cat: "extrovert",
+      },
+      {
+        text: "Hardly notice them at all",
+        cat: "introvert",
+      },
+      {
+        text: "Shoot some friendly smiles in their direction",
+        cat: "extrovert",
+      },
+    ],
+  },
+  {
+    id: 10,
+    question:
+      "At work, somebody asks for your help for the hundredth time. You:",
+    answers: [
+      {
+        text: "Give them a hand, as usual",
+        cat: "introvert",
+      },
+      {
+        text: "Ask them, please, to find somebody else for a change",
+        cat: "extrovert",
+      },
+      {
+        text: "Accept — you're known for being helpful",
+        cat: "introvert",
+      },
+      {
+        text: "Loudly make it known that you're annoyed",
+        cat: "extrovert",
+      },
+    ],
+  },
+];
+
+module.exports = questions;
